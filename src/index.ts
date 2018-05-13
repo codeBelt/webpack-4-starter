5ce131fe61c2d7c120350558f771de75e63a4453
Robert S <rsavian@nerdery.com>
2018-05-13 10:58:05 -0500

    add bootstrap, compile scss and css files

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import './scss/style.scss'
+import 'bootstrap/dist/css/bootstrap.css';
+import './scss/style.scss';
 
-const button = document.createElement("button")
-button.textContent = 'Open chat'
-document.body.appendChild(button)
+const button = document.createElement('button');
+button.textContent = 'Open chat';
+document.body.appendChild(button);
 
 button.onclick = () => {
-    import(/* webpackChunkName: "chat" */ "./chat").then(chat => {
+    import(/* webpackChunkName: 'chat' */ './chat').then(chat => {
         chat.init()
     })
-}
+};
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+// https://www.sitepoint.com/beginners-guide-webpack-module-bundling/
 
 module.exports = {
+
     entry: {
         main: './src/index.ts',
     },
+
     output: {
         // pathinfo: false,
         filename: '[name].bundle.js', // '[name].[chunkhash].js'
         chunkFilename: '[name].[chunkhash].js',
         path: path.resolve(__dirname, 'dist')
     },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+
     module: {
         rules: [
             {
@@ -25,12 +35,13 @@ module.exports = {
                 ],
             },
             {
-                test: /\.scss$/,
+                test: /\.s?css$/,
                 use: [{loader: 'style-loader'}, {loader: 'css-loader'}, {loader: 'sass-loader'}]
             }
         ]
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    }
+
+    plugins: [
+    ]
+
 };
