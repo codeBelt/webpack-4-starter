b021272444547a43d01719dc30ee871874b9cfa0
Robert S <rsavian@nerdery.com>
2018-05-23 08:03:00 -0500

    styles

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,11 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/bootstrap-custom.scss';
 import '../styles/style.scss';
 
 import App from './views/App';
 
 (async (window: Window) => {
 
-    const app = new App();
+    // const app = new App();
 
     if (module.hot) {
         module.hot.accept()
